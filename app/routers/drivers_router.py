f0caf5281653073e765a5fbfdb42b8b50288dddc
Haroon Khan <mrharoonkhan11@gmail.com>
2025-11-03 11:29:50 +0300

    Minor adjustment

--- app/routers/drivers_router.py
+++ app/routers/drivers_router.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter, Query
 from typing import List, Optional
 from datetime import datetime
 
-router = APIRouter(prefix="/api/driver/", tags=["Drivers"])
+router = APIRouter(prefix="/api/driver", tags=["Drivers"])
 
 @router.get("/test_api")
 async def generate_driver_report():
